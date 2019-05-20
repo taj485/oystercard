544bc1ac5fef44b1e1e636a9872bf308a26679ae
Taj <tajbi21@gmail.com>
2019-05-20 15:03:37 +0100

    initialized card with 0 balance

---
+++ lib/oystercard.rb
@@ -0,0 +1,8 @@
+class Oystercard
+  attr_reader :balance
+
+  def initialize(balance = 0)
+    @balance = balance
+  end
+
+end
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -1,2 +1,23 @@
+require 'oystercard.rb'
+
 describe Oystercard do
+let(:card) {Oystercard.new}
+
+  it "can create an instance of it self with default balance of 0" do
+    expect(card.balance).to eql(0)
+  end
+
+
+ #money on card
+ #add money to my card
+ #dont want to put to much money on my card
+ #need fare deducted from ym card
+ #tounch in and touch out to get through barriers
+ #need to have a minimum ammount for a single journey
+ #pay for journey when complete
+ #need to know where i have travled from
+ #see all previous trips
+ #what zone a station is in
+ #penalty charge deducted if i fail to touch in or out
+ #need to have the correct fare calcualted
 end
