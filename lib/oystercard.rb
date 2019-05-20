b290fef25784d45335a265221fbe1fc14bce1b09
Peter Cole <11048765+peterjcole@users.noreply.github.com>
2019-05-20 16:51:02 +0100

    refactor top up balance limit

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -1,13 +1,27 @@
+#!/bin/env ruby
+# encoding: utf-8
+
 class Oystercard
   attr_reader :balance
+  BALANCE_LIMIT = 90
 
   def initialize(balance = 0)
     @balance = balance
   end
 
   def top_up(amount)
-    raise "You can only top up maximum £90 on your oystercard" if amount > 90
-    @balance += amount if @balance < 90
-    raise "you can only have a maximum credit of £90" if @balance > 90
+    raise "You can only top up maximum £90 on your oystercard" if amount > BALANCE_LIMIT
+    if under_balance_limit(amount)
+      @balance += amount
+    else 
+      raise "you can only have a maximum credit of £90"
+    end
   end
+
+  private
+
+  def under_balance_limit(amount)
+    (@balance + amount) < BALANCE_LIMIT
+  end
+
 end
